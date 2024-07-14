5291290e73219d4da70283f0fe5159e9ba4ebcc0
UserPierreG <pierrelouisguichard@gmail.com>
2024-07-14 16:40:39 +0100

    v0.5 - Login hidden atm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Card from "./pages/Card";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/card" element={<Card />} />
+        <Route path="/" element={<Card />} />
+        {/* <Route path="/card" element={<Card />} /> */}
       </Routes>
     </>
   );
