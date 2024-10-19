d09c2977827c226b9a42ba41c72baf12d61ce423
Pierre Guichard <117652420+pierrelouisguichard@users.noreply.github.com>
2024-10-19 19:53:12 +0100

    updated wrong url

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const useLogin = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -10,7 +12,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:4000/api/user/login", {
+    const response = await fetch(`${API_URL}/api/user/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
